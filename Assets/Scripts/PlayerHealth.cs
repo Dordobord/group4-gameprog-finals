00a9fa8f6877d2990de687f791cb707836494593
Dordobord <angelo.dorado@ciit.edu.ph>
2023-04-24 16:33:34 +0800

    -Added a working healthbar that we can slap into any enemy and player prefabs.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public Healthbar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // HealthBar Tester
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.setHealth(currentHealth);
+    }
+}
